c5d8a0513c56019270ca2a16eddb25613e0e82ef
Aaron Meyer <aaron@asmlab.org>
2020-02-09 15:37:40 -0800

    Last files.

---
+++ select/figures/figureCommon.py
@@ -0,0 +1,38 @@
+"""
+Contains utilities and functions that are commonly used in the figure creation files.
+"""
+
+from matplotlib import gridspec, pyplot as plt
+import seaborn as sns
+
+
+def getSetup(figsize, gridd, mults=None, multz=None, empts=None):
+    """ Figure setup. """
+
+    sns.set(style="whitegrid",
+            font_scale=0.7,
+            color_codes=True,
+            palette="colorblind",
+            rc={'grid.linestyle':'dotted',
+                'axes.linewidth':0.6})
+
+    # Setup plotting space
+    f = plt.figure(figsize=figsize)
+
+    # Make grid
+    gs1 = gridspec.GridSpec(*gridd)
+
+    # Get list of axis objects
+    if mults is None:
+        ax = [f.add_subplot(gs1[x]) for x in range(gridd[0] * gridd[1])]
+    else:
+        ax = [f.add_subplot(gs1[x]) if x not in mults else f.add_subplot(gs1[x:x + multz[x]]) for x in range(
+            gridd[0] * gridd[1]) if not any([x - j in mults for j in range(1, max(multz.values()))]) and x not in empts]
+
+    return (ax, f)
+
+
+def subplotLabel(ax, letter, hstretch=1):
+    """Sublot labels"""
+    ax.text(-0.2 / hstretch, 1.2, letter, transform=ax.transAxes,
+            fontsize=16, fontweight='bold', va='top')
