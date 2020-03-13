4c1814c7afd5a1d6dbafa55cb58ce7d4c2bbb1a3
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-03-13 13:38:04 -0700

    Made Val Plot

--- selecv/figures/figure3.py
+++ selecv/figures/figure3.py
@@ -1,14 +1,43 @@
 """
-Figure 3.
+Figure 3. Exploration of Valency.
 """
+import numpy as np
 from .figureCommon import subplotLabel, getSetup
+from ..imports import getPopDict
+from ..sampling import sampleSpec
+
+ligConc = np.array([10e-9])
+KxStarP = 10e3
+affinity = 10e-9
 
 
 def makeFigure():
     """ Make figure 3. """
     # Get list of axis objects
-    ax, f = getSetup((7, 6), (3, 4))
-
+    ax, f = getSetup((7, 6), (4, 3))
     subplotLabel(ax)
 
+    valencyScan = np.array([1, 2, 4, 8, 16, 32])
+    _, populationsdf = getPopDict()
+
+    ValencyPlot(ax[0], populationsdf, valencyScan, ['Pop2', 'Pop3'])
+
     return f
+
+
+def ValencyPlot(ax, df, valencies, popList):
+    df1 = df[df['Population'] == popList[0]]
+    df2 = df[df['Population'] == popList[1]]
+    recMean1 = np.array([df1['Receptor_1'].to_numpy(), df1['Receptor_2'].to_numpy()]).flatten()
+    recMean2 = np.array([df2['Receptor_1'].to_numpy(), df2['Receptor_2'].to_numpy()]).flatten()
+    Cov1 = df1.Covariance_Matrix.to_numpy()[0]
+    Cov2 = df2.Covariance_Matrix.to_numpy()[0]
+    sampMeans, sampDevs = np.zeros([len(valencies)]), np.zeros([len(valencies)])
+    for ii, val in enumerate(valencies):
+        print(val)
+        sampMeans[ii], sampDevs[ii] = sampleSpec(ligConc, KxStarP, val, [recMean1, recMean2], [Cov1, Cov2], np.array([1]), np.array([[affinity, affinity]]), nsample=1000)
+
+    underDev, overDev = sampMeans - sampDevs, sampMeans + sampDevs
+    ax.plot(valencies, sampMeans, color='royalblue')
+    ax.fill_between(valencies, underDev, overDev, color='royalblue', alpha=.1)
+    ax.set(xlabel='Valency', ylabel='Binding Ratio', title=popList[0] + ' to ' + popList[1] + ' binding ratio')
