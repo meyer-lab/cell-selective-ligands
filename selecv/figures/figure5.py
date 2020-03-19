1004ddb965a87e00894656ed589aa90683358bb7
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-03-18 19:02:54 -0700

    Not Working

--- selecv/figures/figure5.py
+++ selecv/figures/figure5.py
@@ -8,23 +8,25 @@ from ..imports import getPopDict
 from ..sampling import sampleSpec
 
 
-xNaught = [10e-9, 10e-9, 1.0, 0.5, 10e-9, 10e-9, 10e-9, 10e-9, ]
-xBnds = ((10e-11, 10e-7), (10e-11, 10e3), (1, 32), (0, 1), (10e-11, 10e-7), (10e-11, 10e-7), (10e-11, 10e-7), (10e-11, 10e-7))
+xNaught = [10e-9, 10e-9, 1.0, 0.5, 10e-7, 10e-11, 10e-11, 10e-7] #Conc, KxStar, Valency, Mix1, aff11, aff12, aff21, aff22
+xBnds = ((10e-11, 10e-7), (10e-11, 10e3), (1, 32), (0, 1), (10e-7, 10e-7), (10e-11, 10e-11), (10e-11, 10e-11), (10e-7, 10e-7))
 
 
 def makeFigure():
     """ Make figure 5. """
     # Get list of axis objects
     ax, f = getSetup((7, 6), (3, 4))
-
     subplotLabel(ax)
+
+    _, populationsdf = getPopDict()
+    optimizeDesign(populationsdf, ['Pop3', 'Pop4'])
 
     return f
 
 
 def minSelecFunc(x, recMeansM, CovsM):
     "Provides the function to be minimized to get optimal selectivity"
-    return 1 / sampleSpec(x[0], x[1], x[2], recMeans, Covs, np.array([x[3], 1 - x[3]]), np.array([[x[4], x[5]], [x[6], x[7]]]))[1]
+    return 1 / sampleSpec(x[0], x[1], x[2], recMeansM, CovsM, np.array([x[3], 1 - x[3]]), np.array([[x[4], x[5]], [x[6], x[7]]]))[1]
 
 
 def optimizeDesign(df, popList):
@@ -35,8 +37,9 @@ def optimizeDesign(df, popList):
         recMeans.append(np.array([dfPop['Receptor_1'].to_numpy(), dfPop['Receptor_2'].to_numpy()]).flatten())
         Covs.append(dfPop.Covariance_Matrix.to_numpy()[0])
     
-    optimized = minimize(minSelecFunc, xNaught, bounds=xBnds, method='L-BFGS-B', args=(reMeans, Covs))
+    optimized = minimize(minSelecFunc, xNaught, bounds=xBnds, method='L-BFGS-B', args=(recMeans, Covs), options={'disp': True})
     print(optimized.x)
-    print(minSelecFunc(optimized.x, recMeans, Covs)^-1)
-    print(minSelecFunc(xNaught, recMeans, Covs)^-1)
+    print(minSelecFunc(optimized.x, recMeans, Covs))
+    print(minSelecFunc(xNaught, recMeans, Covs))
+    print("Conc, KxStar, Valency, Mix1, aff11, aff12, aff21, aff22")
     return optimized
