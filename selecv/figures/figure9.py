c2d79d453a6fbf05e08c5054b4ac90f7ba91ba8e
ctan <cyztan@gmail.com>
2020-04-02 02:08:54 -0700

    General Heatmap setup

--- selecv/figures/figure9.py
+++ selecv/figures/figure9.py
@@ -3,29 +3,33 @@
 """
 
 from .figureCommon import getSetup
-
+from ..model import polyfc, polyc
 
 import numpy as np
 import pandas as pd
 
 
-def makeFigure(L0=1e-9, KxStar=10**-12.2, ff=4, offdiag=1e5, comp=[[4, 0], [0, 4]]):
+def makeFigure(L0, KxStar, Comp, ff = None, Cplx = None, offdiag = 1e5):
     nAffPts = 3
     ax, fig = getSetup((7, 6), (nAffPts, nAffPts))
-    affRange = (5., 7.)
+    affRange = (6., 8.)
     affScan = np.logspace(affRange[0], affRange[1], nAffPts)
     abundRange = (1., 4.)
+    if ff is not None:
+        assert len(Comp) == 2, "Take two types of ligands"
+    else:
+        assert Cplx.shape[1] == 2, "Take two types of ligands"
 
     for i1, aff1 in enumerate(affScan):
         for i2, aff2 in enumerate(np.flip(affScan)):
-            abundHeatMap(ax[i2 * nAffPts + i1], abundRange, [[aff1, offdiag], [offdiag, aff2]],
-                         L0=L0, KxStar=KxStar, f=ff, comp=comp)
+            abundHeatMap(ax[i2 * nAffPts + i1], abundRange, L0, KxStar, \
+                         [[aff1, offdiag], [offdiag, aff2]], Comp, f = ff, Cplx = Cplx)
 
     return fig
 
-
-def abundHeatMap(ax, abundRange, Kav, L0=1e-9, KxStar=10**-12.2, f=4, comp=[[4, 0], [0, 4]]):
+def abundHeatMap(ax, abundRange, L0, KxStar, Kav, Comp, f = None, Cplx = None):
     "Makes a heatmap comparing bindings of populations at multiple receptor abundances"
+    assert bool(f is None) != bool(Cplx is None)
     nAbdPts = 70
     abundScan = np.logspace(abundRange[0], abundRange[1], nAbdPts)
     bindDF = pd.DataFrame(columns=abundScan, index=np.flip(abundScan))
@@ -33,9 +37,12 @@ def abundHeatMap(ax, abundRange, Kav, L0=1e-9, KxStar=10**-12.2, f=4, comp=[[4, 
     for i1, abund1 in enumerate(abundScan):
         sampMeans = np.zeros(nAbdPts)
         for i2, abund2 in enumerate(np.flip(abundScan)):
-            #sampMeans[i2] = polyfc(L0, KxStar, f, [abund1, abund2], comp, Kav)
-            sampMeans[i2] = polyc(L0 / np.sum(comp) * 4, KxStar, [abund1, abund2], comp, [0.5, 0.5], Kav)
+            if f is None:
+                sampMeans[i2] = polyc(L0, KxStar, [abund1, abund2], Cplx, Comp, Kav)[0]
+            else:
+                sampMeans[i2] = polyfc(L0, KxStar, f, [abund1, abund2], Comp, Kav)[0]
+
         bindDF[bindDF.columns[i1]] = np.log(sampMeans)
 
-    sns.heatmap(bindDF, ax=ax, xticklabels=nAbdPts - 1, yticklabels=nAbdPts - 1, vmin=1, vmax=6)
+    sns.heatmap(bindDF, ax=ax, xticklabels=nAbdPts - 1, yticklabels=nAbdPts - 1, vmin=0, vmax=6)
     ax.set(xlabel='Rec 1 abundance', ylabel='Rec 2 abundance')
