cfba2bb7ece856e33f3271aa6d7e56eb115e40a9
Cyrillus Tan <cyztan@gmail.com>
2020-04-03 10:23:22 -0700

    Name change for L0 to avoid confusion (#46)
    * Edit .gitignore
    
    * Name change

--- selecv/model.py
+++ selecv/model.py
@@ -8,16 +8,16 @@ from scipy.optimize import root
 
 
 @njit
-def Req_func(Req, Rtot, L0fA, AKxStar, f):
+def Req_func(Req, Rtot, LmonoA, AKxStar, f):
     """ Mass balance. Transformation to account for bounds. """
     Phisum = np.dot(AKxStar, Req.T)
-    return Req + L0fA * Req * (1 + Phisum) ** (f - 1) - Rtot
+    return Req + LmonoA * Req * (1 + Phisum) ** (f - 1) - Rtot
 
 
-def polyfc(L0, KxStar, f, Rtot, LigC, Kav):
+def polyfc(Lmono, KxStar, f, Rtot, LigC, Kav):
     """
     The main function. Generate all info for heterogenenous binding case
-    L0: concentration of ligand.
+    Lmono: concentration of ligand monomers.
     KxStar: detailed balance-corrected Kx.
     f: valency
     Rtot: numbers of each receptor appearing on the cell.
@@ -25,7 +25,6 @@ def polyfc(L0, KxStar, f, Rtot, LigC, Kav):
     Kav: a matrix of Ka values. row = IgG's, col = FcgR's
     """
     # Data consistency check
-    L0 = L0 / f
     Kav = np.array(Kav)
     Rtot = np.array(Rtot)
     assert Rtot.ndim <= 1
@@ -37,7 +36,7 @@ def polyfc(L0, KxStar, f, Rtot, LigC, Kav):
     assert Kav.ndim == 2
 
     # Run least squares to get Req
-    Req = Req_Regression(L0, KxStar, f, Rtot, LigC, Kav)
+    Req = Req_Regression(Lmono, KxStar, f, Rtot, LigC, Kav)
 
     nr = Rtot.size  # the number of different receptors
 
@@ -45,8 +44,8 @@ def polyfc(L0, KxStar, f, Rtot, LigC, Kav):
     Phi[:, :nr] *= Kav * Req * KxStar
     Phisum = np.sum(Phi[:, :nr])
 
-    Lbound = L0 / KxStar * ((1 + Phisum)**f - 1)
-    Rbound = L0 / KxStar * f * Phisum * (1 + Phisum)**(f - 1)
+    Lbound = Lmono / f / KxStar * ((1 + Phisum)**f - 1)
+    Rbound = Lmono / KxStar * Phisum * (1 + Phisum)**(f - 1)
 
     return Lbound, Rbound
 
@@ -55,18 +54,18 @@ def polyfcm(KxStar, f, Rtot, Lig, Kav):
     return polyfc(np.sum(Lig) / f, KxStar, f, Rtot, Lig / np.sum(Lig), Kav)
 
 
-def Req_Regression(L0, KxStar, f, Rtot, LigC, Kav):
+def Req_Regression(Lmono, KxStar, f, Rtot, LigC, Kav):
     """Run least squares regression to calculate the Req vector"""
     A = np.dot(LigC.T, Kav)
-    L0fA = L0 * f * A
+    LmonoA = Lmono * A
     AKxStar = A * KxStar
 
     # Identify an initial guess just on max monovalent interaction
-    x0 = np.max(L0fA, axis=0)
+    x0 = np.max(LmonoA, axis=0)
     x0 = np.multiply(1.0 - np.divide(x0, 1 + x0), Rtot)
 
     # Solve Req by calling least_squares() and Req_func()
-    lsq = root(Req_func, x0, method="lm", args=(Rtot, L0fA, AKxStar, f), options={"maxiter": 3000})
+    lsq = root(Req_func, x0, method="lm", args=(Rtot, LmonoA, AKxStar, f), options={"maxiter": 3000})
 
     assert lsq["success"], "Failure in rootfinding. " + str(lsq)
 
