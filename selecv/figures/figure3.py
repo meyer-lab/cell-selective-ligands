33a9f3532fbb39426f5c672f826ae83be69a3f84
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2020-04-30 18:26:20 -0700

    Valency section start (#61)

--- selecv/figures/figure3.py
+++ selecv/figures/figure3.py
@@ -44,16 +44,16 @@ def valDemo(ax):
     colors = ["royalblue", "orange", "limegreen", "orangered"]
     lines = ["-", ":"]
     nPoints = 100
-    recScan = np.logspace(0, 4, nPoints)
+    recScan = np.logspace(0, 8, nPoints)
     labels = ["Monovalent", "Bivalent", "Trivalent", "Tetravalent"]
     percHold = np.zeros(nPoints)
     for ii, aff in enumerate(affs):
         for jj, valencyLab in enumerate(labels):
             for kk, recCount in enumerate(recScan):
-                percHold[kk] = polyfc(ligConc / (jj + 1), KxStarP, jj + 1, recCount, [1], np.array([[aff]]))[1] / recCount
+                percHold[kk] = polyfc(ligConc / (jj + 1), KxStarP, jj + 1, recCount, [1], np.array([[aff]]))[0] / recCount
             ax.plot(recScan, percHold, label=valencyLab, linestyle=lines[ii], color=colors[jj])
 
-    ax.set(xlim=(1, 10000), ylim=(0, 1), xlabel="Receptor Abundance", ylabel="Fraction Receptors Bound", xscale="log")
+    ax.set(xlim=(1, 100000000), xlabel="Receptor Abundance", ylabel="Lig bound / Receptor", xscale="log") # ylim=(0, 1), 
     handles, _ = ax.get_legend_handles_labels()
     handles = handles[0:4]
     line = Line2D([], [], color="black", marker="_", linestyle="None", markersize=6, label="High Affinity")
@@ -67,13 +67,13 @@ def ConcValPlot(ax):
     "Keep valency constant and high - vary concentration"
     concScan = np.logspace(-11, -7, 5)
     valency = 4
-    recScan = np.logspace(0, 4, 100)
+    recScan = np.logspace(0, 8, 100)
     percHold = np.zeros(100)
 
     for conc in concScan:
         for jj, recCount in enumerate(recScan):
-            percHold[jj] = polyfc(conc / valency, KxStarP, valency, recCount, [1], np.array([[affinity]]))[1] / recCount
+            percHold[jj] = polyfc(conc / valency, KxStarP, valency, recCount, [1], np.array([[affinity]]))[0] / recCount
         ax.plot(recScan, percHold, label=str(conc * 10e8) + " nM")
 
-    ax.set(xlim=(1, 10000), ylim=(0, 1), xlabel="Receptor Abundance", ylabel="Fraction Receptors Bound", xscale="log")
+    ax.set(xlim=(1, 100000000), xlabel="Receptor Abundance", ylabel="Lig Bound / Receptor", xscale="log")#ylim=(0, 1), 
     ax.legend(prop={"size": 6})
