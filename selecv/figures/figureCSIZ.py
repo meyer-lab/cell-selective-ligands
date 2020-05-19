0054f88ec4a9149787af05df35734f824eb12b55
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-05-18 17:51:20 -0700

    Fitting going

---
+++ selecv/figures/figureCSIZ.py
@@ -0,0 +1,28 @@
+"""
+Figure 6. PolyC vs PolyFc.
+"""
+
+import numpy as np
+from .figureCommon import subplotLabel, getSetup, PlotCellPops
+from ..imports import getPopDict
+from ..sampling import sampleSpec, sampleSpecC
+from ..csizmar import model_predict, fit_slope, discrim, discrim2, fitfunc, xeno
+
+ligConc = np.array([10e-10])
+# 10**-12.734, KxStar_C5 = 10**-14.693, KxStar = 10**-14.714
+KxStarP = KxStar = 10**-12.734
+
+
+def makeFigure():
+    """ Make figure 4. """
+    # Get list of axis objects
+    ax, f = getSetup((7, 6), (2, 3))
+    subplotLabel(ax)
+    fit = fitfunc()
+    FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund = np.exp(fit[0]), fit[1], fit[2], [[np.exp(fit[3])], [np.exp(fit[4])], [0]], np.exp(fit[5])
+
+    fit_slope(ax[0], FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund)
+    discrim2(ax[1], FitKX, FitSlopeC5, FitSlopeB22)
+    xeno(ax[2], FitKX, Kav)
+
+    return f
