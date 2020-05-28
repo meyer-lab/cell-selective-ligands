e7e9121f47d10bd73ad778e5c0ad8835a7df0a6d
ctan <cyztan@gmail.com>
2020-05-27 20:05:30 -0700

    Measurement is just Lbound

--- selecv/csizmar.py
+++ selecv/csizmar.py
@@ -60,14 +60,14 @@ slope_C5 = 0.008514426941736077
 slope_B22 = 0.012855332053729724
 
 
-def discrim():
+def discrim(KxStar, slope):
     "Returns predicted fluorescent values over a range of abundances"
     df = pd.DataFrame(columns=["Lig", "Recep", "value"])
     for lig in [[8, 0, 0], [4, 0, 4], [0, 8, 0], [0, 4, 4]]:
         for rec in Recep.values():
             res = polyfc(50 * 1e-9, KxStar, 8, [rec], lig, Kav)
             Lbound, _ = res[0], res[1]
-            df = df.append({"Lig": str(lig), "Recep": rec, "value": Lbound * slope * (lig[0] + lig[1])}, ignore_index=True)
+            df = df.append({"Lig": str(lig), "Recep": rec, "value": Lbound * slope}, ignore_index=True)
     ax = sns.lineplot(x="Recep", y="value", hue="Lig", style="Lig", markers=True, data=df)
     ax.set_xscale("log")
     ax.set_yscale("log")
@@ -81,11 +81,11 @@ def discrim2(ax, KxStarD, slopeC5, slopeB22, KavD):
     for lig in [[8, 0, 0], [4, 0, 4]]:
         for rec in Recep.values():
             res = polyfc(50 * 1e-9, KxStarD, 8, [rec], lig, KavD)
-            df = df.append({"Lig": str(lig), "Recep": rec, "value": res[0] * slopeC5}, ignore_index=True)  # * (lig[0] + lig[1])
+            df = df.append({"Lig": str(lig), "Recep": rec, "value": res[0] * slopeC5}, ignore_index=True)
     for lig in [[0, 8, 0], [0, 4, 4]]:
         for rec in Recep.values():
             res = polyfc(50 * 1e-9, KxStarD, 8, [rec], lig, KavD)
-            df = df.append({"Lig": str(lig), "Recep": rec, "value": res[0] * slopeB22}, ignore_index=True)  # * (lig[0] + lig[1])
+            df = df.append({"Lig": str(lig), "Recep": rec, "value": res[0] * slopeB22}, ignore_index=True)
     sns.lineplot(x="Recep", y="value", hue="Lig", style="Lig", markers=True, data=df, ax=ax)
     ax.set_xscale("log")
     ax.set_yscale("log")
