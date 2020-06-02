a57f63d5285189596306f9a5d0bf89e35d4b37e1
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-06-01 23:18:19 -0700

    Valency in optimization

--- selecv/figures/figureCSIZ.py
+++ selecv/figures/figureCSIZ.py
@@ -14,9 +14,11 @@ def makeFigure():
     subplotLabel(ax)
     fit = fitfunc()
     FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund = np.exp(fit[0]), fit[1], fit[2], [[np.exp(fit[3])], [np.exp(fit[4])], [0]], np.exp(fit[5])
+    valencies = np.array([fit[6], fit[7], fit[8], fit[9]])
+    print(valencies)
 
-    fit_slope(ax[0], FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund)
-    discrim2(ax[1], FitKX, FitSlopeC5, FitSlopeB22, Kav)
-    xeno(ax[2], FitKX, Kav)
+    fit_slope(ax[0], FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund, valencies)
+    #discrim2(ax[1], FitKX, FitSlopeC5, FitSlopeB22, Kav, [valencies[3], valencies[2]])
+    #xeno(ax[2], FitKX, Kav)
 
     return f
