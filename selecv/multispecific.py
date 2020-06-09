c909d681e55288c50522109ffbc57c22831a75f2
ctan <cyztan@gmail.com>
2020-06-09 12:37:49 -0700

    Bispecific with different KxStar

--- selecv/multispecific.py
+++ selecv/multispecific.py
@@ -10,6 +10,8 @@ Cplx = np.array([[1, 1]])
 Ctheta = [1.0]
 Kav = np.array([[1e6, 5e4], [5e4, 1e6]])
 
-Lbound1, _ = polyc(L0, KxStar, Rtot1, Cplx, Ctheta, Kav)
-Lbound2, _ = polyc(L0, KxStar, Rtot2, Cplx, Ctheta, Kav)
-ratio = Lbound1[0] / Lbound2[0]
+for KxStar in np.logspace(-15, -9, num = 7):
+    Lbound1, _ = polyc(L0, KxStar, Rtot1, Cplx, Ctheta, Kav)
+    Lbound2, _ = polyc(L0, KxStar, Rtot2, Cplx, Ctheta, Kav)
+    ratio = Lbound1[0] / Lbound2[0]
+    print(KxStar, ratio)
