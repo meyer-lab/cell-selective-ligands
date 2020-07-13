b8bd46d5909995d339b3fa31364c1a7781225ddc
ctan <cyztan@gmail.com>
2020-07-13 15:30:39 -0700

    Restructure BOJ optimizer to be more general purpose

--- selecv/figures/figure6.py
+++ selecv/figures/figure6.py
@@ -17,14 +17,14 @@ def makeFigure():
     ax, f = getSetup((16, 8), (3, 6))
     subplotLabel(ax)
 
-    _, populationsdf = getPopDict()
     # gridSearchTry(populationsdf, ['Pop5', 'Pop3'])
-    optimizeDesign(ax[0:6], populationsdf, [r"$R_1^{lo}R_2^{hi}$"])
-    optimizeDesign(ax[6:12], populationsdf, [r"$R_1^{hi}R_2^{hi}$"])
-    optimizeDesign(ax[12:18], populationsdf, [r"$R_1^{med}R_2^{med}$"])
+    optimizeDesign(ax[0:6], [r"$R_1^{lo}R_2^{hi}$"])
+    optimizeDesign(ax[6:12], [r"$R_1^{hi}R_2^{hi}$"])
+    optimizeDesign(ax[12:18], [r"$R_1^{med}R_2^{med}$"])
 
     return f
 
+_, df = getPopDict()
 
 def minSelecFunc(x, tMeans, offTMeans):
     "Provides the function to be minimized to get optimal selectivity"
@@ -37,9 +37,7 @@ def minSelecFunc(x, tMeans, offTMeans):
 
     return (offTargetBound) / (targetBound)
 
-
-def optimizeDesign(ax, df, targetPop):
-    "Runs optimization and determines optimal parameters for selectivity of one population vs. another"
+def genOnevsAll(targetPop):
     targMeans, offTargMeans = [], []
     for _, pop in enumerate(df["Population"].unique()):
         dfPop = df[df["Population"] == pop]
@@ -47,14 +45,34 @@ def optimizeDesign(ax, df, targetPop):
             targMeans.append(np.array([dfPop["Receptor_1"].to_numpy(), dfPop["Receptor_2"].to_numpy()]).flatten())
         else:
             offTargMeans.append(np.array([dfPop["Receptor_1"].to_numpy(), dfPop["Receptor_2"].to_numpy()]).flatten())
+    return targMeans, offTargMeans
+
+
+def optimize(pmOptNo, targMeans, offTargMeans, L0, KxStar, f, LigC, Kav, bound = None):
+    """ A more general purpose optimizer """
+    # OPT = [log L0, log KxStar, f, LigC[0], log Ka(diag), log Ka(offdiag)]
+    Kav = np.array(Kav)
+    xnot = np.array([np.log(L0), np.log(KxStar), f, LigC[0], np.log(Kav[0, 0]), np.log(Kav[0, 1])])
+    Bnds = [(i, i) for i in xnot]
+    for pmopt in pmOptNo:
+        Bnds[pmopt] = optBnds[pmopt]
+    if bound != None:
+        Bnds = bound
+    optimized = minimize(minSelecFunc, xnot, bounds=np.array(Bnds), method="L-BFGS-B", args=(targMeans, offTargMeans),
+                        options={"eps": 1, "disp": True})
+    return optimized
+
+
+def optimizeDesign(ax, targetPop):
+    "Runs optimization and determines optimal parameters for selectivity of one population vs. another"
+    targMeans, offTargMeans = genOnevsAll(targetPop)
 
     optDF = pd.DataFrame(columns=["Strategy", "Selectivity"])
     strats = ["Xnot", "Affinity", "Mixture", "Valency", "All"]
-    xnot = np.array([np.log(1e-9), np.log(1e-12), 1, 1, np.log(1e6), np.log(1e6)])
+    pmOpts = [[], [1, 4, 5], [1, 3], [1, 3], [1, 3, 4, 5]]
 
     for i, strat in enumerate(strats):
-        xBnds = bndsDict[strat]
-        optimized = minimize(minSelecFunc, xnot, bounds=xBnds, method="L-BFGS-B", args=(targMeans, offTargMeans), options={"eps": 1, "disp": True})
+        optimized = optimize(pmOpts[i], targMeans, offTargMeans, 1e-9, 1e-12, 1, [1, 0], np.ones((2,2))*1e6, bound = bndsDict[strat])
         stratRow = pd.DataFrame({"Strategy": strat, "Selectivity": np.array([len(offTargMeans) / optimized.fun])})
         optDF = optDF.append(stratRow, ignore_index=True)
         optParams = optimized.x
@@ -68,6 +86,14 @@ def optimizeDesign(ax, df, targetPop):
 
     sns.barplot(x="Strategy", y="Selectivity", data=optDF, ax=ax[0])
     ax[0].set(title="Optimization of " + targetPop[0])
+
+
+optBnds = [(np.log(1e-11), np.log(1e-8)),  # log L0
+           (np.log(1e-15), np.log(1e-9)),  # log KxStar
+           (1, 16),  # f
+           (0.0, 1.0),  # LigC[0]
+           (np.log(1e2), np.log(1e9)),  # log Ka(diag)
+           (np.log(1e2), np.log(1e9))]  # log Ka(offdiag)
 
 
 bndsDict = {
