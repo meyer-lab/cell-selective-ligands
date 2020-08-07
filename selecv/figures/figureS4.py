8936b4a5398b5cb753cae55edd0dfacd66beaa4d
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-08-07 14:30:03 -0700

    Start S4 expansion

--- selecv/figures/figureS4.py
+++ selecv/figures/figureS4.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.optimize import minimize
-from .figureCommon import subplotLabel, getSetup
+from .figureCommon import subplotLabel, getSetup, cellPopulations, overlapCellPopulations
 from ..imports import getPopDict
 from .figure6 import genOnevsAll
 from ..model import polyc
@@ -14,15 +14,20 @@ from ..model import polyc
 def makeFigure():
     """ Make figure S4. """
     # Get list of axis objects
-    ax, f = getSetup((18, 6), (1, 3))
+    ax, f = getSetup((18, 12), (2, 3))
     subplotLabel(ax)
     affDLsub = np.array([0, 15])
-    fDLsub = 1
+    fDLsub = 4
 
     # gridSearchTry(populationsdf, ['Pop5', 'Pop3'])
     optimizeDesignDL(ax[0], [r"$R_1^{lo}R_2^{hi}$"], fDLsub, affDLsub)
     optimizeDesignDL(ax[1], [r"$R_1^{hi}R_2^{hi}$"], fDLsub, affDLsub)
     optimizeDesignDL(ax[2], [r"$R_1^{med}R_2^{med}$"], fDLsub, affDLsub)
+
+    fDLsub = 4
+    optimizeDesignDL(ax[3], [r"$R_1^{lo}R_2^{hi}$"], fDLsub, affDLsub)
+    optimizeDesignDL(ax[4], [r"$R_1^{hi}R_2^{hi}$"], fDLsub, affDLsub)
+    optimizeDesignDL(ax[5], [r"$R_1^{med}R_2^{med}$"], fDLsub, affDLsub)
 
     return f
 
@@ -70,3 +75,45 @@ def optimizeDesignDL(ax, targetPop, fDL, affDL):
 
     ax.set(xlabel="Dead Ligand Rec 1 Affinity ($K_a$, in M$^{-1}$)", ylabel="Dead Ligand Rec 2 Affinity ($K_a$, in M$^{-1}$)")
     ax.set_title(targetPop, fontsize=8)
+
+    maxindices = argmax(ratioDF.to_numpy())
+    maxaff1 = affscan[maxindices[0]]
+    maxaff2 = affscan[-1*maxindices[1]]
+    optimized = minimize(minSelecFuncDL, xnot, bounds=np.array(bounds), method="L-BFGS-B", args=(targMeans, offTargMeans, fDL, [maxaff1, maxaff2]), options={"eps": 1, "disp": True})
+
+    return optimized.x, np.array([maxaff1, maxaff2])
+
+
+def modifyCellPops(cellPopsOriginal, optLig, dLigAff, fDL):
+    "Modify cell pops by amount of dead ligand binding"
+    x = optLig
+    for i, row in cellPopsOriginal.items():
+        Rbound = polyc(np.exp(x[0]), np.exp(x[1]), [10**row[0], 10**row[2]], [[fDL, 0], [0, x[2]]], [0.5, 0.5], np.array([[dLigAff[0], dLigAff[1]], [np.exp(x[4]), np.exp(x[5])]]))[1][0]
+        row[0:2] = row[0:2] - Rbound
+        cellPopsOriginal[i] = row
+    return cellPopsOriginal
+
+
+def heatmapDL(ax, L0, KxStar, Kav, Comp, f=None, Cplx=None, vrange=(-2, 4), title="", cbar=False, layover=True, fully=False):
+    assert bool(f is None) != bool(Cplx is None)
+    nAbdPts = 70
+    abundRange = (1.5, 4.5)
+    abundScan = np.logspace(abundRange[0], abundRange[1], nAbdPts)
+
+    func = np.vectorize(lambda abund1, abund2: polyfc(L0, KxStar, f, [abund1, abund2], Comp, Kav)[0])
+
+    X, Y = np.meshgrid(abundScan, abundScan)
+    logZ = np.log(func(X, Y))
+
+    contours = ax.contour(X, Y, logZ, levels=np.arange(-20, 20, 0.5), colors="black", linewidths=0.5)
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_title(title)
+    plt.clabel(contours, inline=True, fontsize=6)
+    ax.pcolor(X, Y, logZ, cmap='RdYlGn', vmin=vrange[0], vmax=vrange[1])
+    norm = plt.Normalize(vmin=vrange[0], vmax=vrange[1])
+    if cbar:
+        cbar = ax.figure.colorbar(cm.ScalarMappable(norm=norm, cmap='RdYlGn'), ax=ax)
+        cbar.set_label("Log Ligand Bound")
+    if layover:
+        overlapCellPopulation(ax, abundRange)
