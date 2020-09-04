c49ef99c70e22d51e92b9ca8a59bd2083676326b
ctan <cyztan@gmail.com>
2020-09-04 13:44:26 -0700

    More PyLint fix

--- selecv/tests/test_sigmaPts.py
+++ selecv/tests/test_sigmaPts.py
@@ -10,6 +10,6 @@ class TestSigmaPts(unittest.TestCase):
 
             x = np.array([np.log(1e-9), np.log(1e-13), 5, 0.8, np.log(1e7), np.log(1e5)])
             sel1 = minSelecFunc(x, targPops, offTargPops)
-            sel2 = minSigmaVar(x, targPops, offTargPops, h = 0)
+            sel2 = minSigmaVar(x, targPops, offTargPops, h=0)
 
-            self.assertAlmostEqual(sel1 ,sel2)
+            self.assertAlmostEqual(sel1, sel2)
