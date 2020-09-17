9ec0a1b637c324c4c07c497e10c9f9d7f6816789
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-09-17 11:45:56 -0700

    Build (#154)
    * Setup build.
    
    * Remove coverage from Jenkins
    
    * Rename Jenkins step

--- selecv/tests/test_sigmaPts.py
+++ selecv/tests/test_sigmaPts.py
@@ -1,16 +1,13 @@
-import unittest
+import pytest
 import numpy as np
 from selecv.sampling import cellPopulations
 from selecv.figures.figure6 import genOnevsAll, minSelecFunc, minSigmaVar
 
 
-class TestSigmaPts(unittest.TestCase):
-    def test_hAsZero(self):
-        for targetPop in cellPopulations.keys():
-            targPops, offTargPops = genOnevsAll([targetPop])
-
-            x = np.array([np.log(1e-9), np.log(1e-13), 5, 0.8, np.log(1e7), np.log(1e5)])
-            sel1 = minSelecFunc(x, targPops, offTargPops)
-            sel2 = minSigmaVar(x, targPops, offTargPops, h=0)
-
-            self.assertAlmostEqual(sel1, sel2)
+@pytest.mark.parametrize("targetPop", cellPopulations.keys())
+def test_hAsZero(targetPop):
+    targPops, offTargPops = genOnevsAll([targetPop])
+    x = np.array([np.log(1e-9), np.log(1e-13), 5, 0.8, np.log(1e7), np.log(1e5)])
+    sel1 = minSelecFunc(x, targPops, offTargPops)
+    sel2 = minSigmaVar(x, targPops, offTargPops, h=0)
+    np.testing.assert_almost_equal(sel1, sel2)
