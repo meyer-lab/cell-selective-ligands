49fa7fa569241da8d1b0c04301c358adaf618edb
Cyrillus Tan <cyztan@gmail.com>
2020-09-18 18:00:25 -0700

    Turn on sigma point (#155)

--- selecv/sampling.py
+++ selecv/sampling.py
@@ -25,7 +25,7 @@ cellPopulations = {
 
 def sigmapts(name, h=None):
     if h is None:
-        h = 0  # np.sqrt(3)
+        h = np.sqrt(3)
     l = cellPopulations[name]
     x = np.array([l[0], l[1]])
     rot = np.array([[np.cos(np.deg2rad(l[4])), -np.sin(np.deg2rad(l[4]))], [np.sin(np.deg2rad(l[4])), np.cos(np.deg2rad(l[4]))]])
