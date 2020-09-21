ffe65cb07d3ad6b6c6a77820aa217fcb1c26728e
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2020-09-21 16:55:24 -0700

    Start (#158)
    * Start
    
    * Fixed?
    
    * More Work
    
    * Working
    
    * Fix Sig Test

--- selecv/tests/test_sigmaPts.py
+++ selecv/tests/test_sigmaPts.py
@@ -7,7 +7,7 @@ from selecv.figures.figure6 import genOnevsAll, minSelecFunc, minSigmaVar
 @pytest.mark.parametrize("targetPop", cellPopulations.keys())
 def test_hAsZero(targetPop):
     targPops, offTargPops = genOnevsAll([targetPop])
-    x = np.array([np.log(1e-9), np.log(1e-13), 5, 0.8, np.log(1e7), np.log(1e5)])
+    x = np.array([np.log(1e-9), np.log(1e-13), 5, 0.8, np.log(1e7), np.log(1e5), np.log(1e7), np.log(1e5)])
     sel1 = minSelecFunc(x, targPops, offTargPops)
     sel2 = minSigmaVar(x, targPops, offTargPops, h=0)
     np.testing.assert_almost_equal(sel1, sel2)
