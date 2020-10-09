a9491b1ed501b58f4ad4a21669b527282bca460f
ctan <cyztan@gmail.com>
2020-10-08 18:02:43 -0700

    CT edit fix Fig 1

--- genFigures.py
+++ genFigures.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
         overlayCartoon(fdir + 'figure1.svg',
                        './selecv/graphics/figure_1a.svg', 20, 10, scalee=0.35, scale_x=0.5, scale_y=0.5)
         overlayCartoon(fdir + 'figure1.svg',
-                       './selecv/graphics/figure_1b.svg', 10, 280, scalee=0.5, scale_x=0.3, scale_y=0.3)
+                       './selecv/graphics/figure_1b.svg', 5, 280, scalee=0.5, scale_x=0.3, scale_y=0.3)
 
     info('%s is done after %s seconds.', nameOut, time() - start)
