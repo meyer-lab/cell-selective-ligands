6f259dfa69bfbe32cd0b94f65250c6ba063277ef
ctan <cyztan@gmail.com>
2020-10-11 18:15:14 -0700

    Small fixes in figures

--- selecv/figures/figure6.py
+++ selecv/figures/figure6.py
@@ -122,7 +122,7 @@ def optimizeDesign(ax, targetPop, vrange=(0, 5)):
     targMeans, offTargMeans = genPopMeans(targPops), genPopMeans(offTargPops)
 
     optDF = pd.DataFrame(columns=["Strategy", "Selectivity"])
-    strats = ["Xnot", "Affinity", "Mixture", "Valency", "All"]
+    strats = ["Xo", "Affinity", "Mixture", "Valency", "All"]
     pmOpts = [[], [1, 4, 5], [1, 3], [1, 3], [1, 3, 4, 5]]
 
     for i, strat in enumerate(strats):
@@ -165,7 +165,7 @@ optBnds = [(np.log(1e-11), np.log(1e-8)),  # log L0
 cBnd = (np.log(1e-9), np.log(1.01e-9))
 
 bndsDict = {
-    "Xnot": (cBnd, (np.log(1e-12), np.log(1.01e-12)), (1, 1.01), (0.99, 1.00), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6))),
+    "Xo": (cBnd, (np.log(1e-12), np.log(1.01e-12)), (1, 1.01), (0.99, 1.00), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6))),
     "Affinity": (cBnd, (np.log(1e-12), np.log(1.01e-12)), (1, 1.01), (0.99, 1.00), (np.log(1e2), np.log(1e10)), (np.log(1e2), np.log(1e10)), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6))),
     "Mixture": (cBnd, (np.log(1e-12), np.log(1.01e-12)), (1, 1.01), (0, 1), (np.log(1e2), np.log(1e10)), (np.log(1e2), np.log(1e10)), (np.log(1e2), np.log(1e10)), (np.log(1e2), np.log(1e10))),
     "Valency": (cBnd, (np.log(1e-15), np.log(1e-9)), (1, 16), (0.99, 1.00), (np.log(1e2), np.log(1e10)), (np.log(1e2), np.log(1e10)), (np.log(1e6), np.log(1.01e6)), (np.log(1e6), np.log(1.01e6))),
@@ -179,7 +179,7 @@ def optimizeDesignAnim(targetPop):
     targMeans, offTargMeans = genPopMeans(targPops), genPopMeans(offTargPops)
 
     optDF = pd.DataFrame(columns=["Strategy", "Selectivity"])
-    strats = ["Xnot", "Affinity", "Mixture", "Valency", "All"]
+    strats = ["Xo", "Affinity", "Mixture", "Valency", "All"]
     pmOpts = [[], [1, 4, 5], [1, 3], [1, 3], [1, 3, 4, 5]]
     optParamsHold = np.zeros([len(strats), 6])
 
