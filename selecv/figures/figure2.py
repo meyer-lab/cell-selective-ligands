0259e00a800489c63214a828a0af1bbbfdae921d
Cyrillus Tan <cyztan@gmail.com>
2020-10-13 18:00:27 -0700

    Figure 4 fix (#173)
    * Sigma point on binding ratio plots
    
    * Figure 4 edits

--- selecv/figures/figure2.py
+++ selecv/figures/figure2.py
@@ -5,7 +5,6 @@ Figure 2. Explore selectivity vs. affinity.
 import numpy as np
 from matplotlib import pyplot as plt
 from .figureCommon import subplotLabel, getSetup, popCompare, heatmap
-from ..imports import getPopDict
 
 ligConc = np.array([1e-9])
 KxStarP = 1e-10
@@ -16,16 +15,15 @@ def makeFigure():
     """ Make figure 2. """
     # Get list of axis objects
     ax, f = getSetup((12, 14), (5, 3))
-    _, populationsdf = getPopDict()
     subplotLabel(ax, [0] + list(range(9, 13)))
     ax[13].axis("off")
     ax[14].axis("off")
     affinity(f, ax[0:9], 1e-9, 1e-10, [1.0], ff=1, vmin=-1, vmax=5.5)
 
-    popCompare(ax[9], [r"$R_1^{hi}R_2^{lo}$", r"$R_1^{lo}R_2^{hi}$"], populationsdf, "Aff", Kav=[5, 7])
-    popCompare(ax[10], [r"$R_1^{med}R_2^{hi}$", r"$R_1^{hi}R_2^{med}$"], populationsdf, "Aff", Kav=[5, 7])
-    popCompare(ax[11], [r"$R_1^{hi}R_2^{lo}$", r"$R_1^{med}R_2^{lo}$"], populationsdf, "Aff", Kav=[5, 7])
-    popCompare(ax[12], [r"$R_1^{hi}R_2^{hi}$", r"$R_1^{med}R_2^{med}$"], populationsdf, "Aff", Kav=[5, 7])
+    popCompare(ax[9], [r"$R_1^{hi}R_2^{lo}$", r"$R_1^{lo}R_2^{hi}$"], "Aff", Kav=[5, 7])
+    popCompare(ax[10], [r"$R_1^{med}R_2^{hi}$", r"$R_1^{hi}R_2^{med}$"], "Aff", Kav=[5, 7])
+    popCompare(ax[11], [r"$R_1^{hi}R_2^{lo}$", r"$R_1^{med}R_2^{lo}$"], "Aff", Kav=[5, 7])
+    popCompare(ax[12], [r"$R_1^{hi}R_2^{hi}$", r"$R_1^{med}R_2^{med}$"], "Aff", Kav=[5, 7])
 
     return f
 
