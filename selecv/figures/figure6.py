052b3ab3d29ff8efab8eb5b957487762ea260929
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-10-16 11:38:42 -0700

    Text fixes and fig 6 fix

--- selecv/figures/figure6.py
+++ selecv/figures/figure6.py
@@ -144,14 +144,13 @@ def optimizeDesign(ax, targetPop, vrange=(0, 5)):
             heatmapNorm(ax[i + 1], targMeans[0], optParams[0], optParams[1], [[optParams[4], optParams[5]], [optParams[6], optParams[7]]],
                         [optParams[3], 1 - optParams[3]], f=optParams[2], vrange=vrange, cbar=False, layover=True, highlight=targetPop[0])
         else:
-            print(optParams[0], optParams[1], [[optParams[4], optParams[5]], [optParams[6], optParams[7]]],
-                  [optParams[3], 1 - optParams[3]], optParams[2])
             heatmapNorm(ax[i + 1], targMeans[0], optParams[0], optParams[1], [[optParams[4], optParams[5]], [optParams[6], optParams[7]]],
                         [optParams[3], 1 - optParams[3]], f=optParams[2], vrange=vrange, cbar=True, layover=True, highlight=targetPop[0])
         ax[i + 1].set(title=strat, xlabel="Receptor 1 Abundance ($cell^{-1}$))", ylabel="Receptor 2 Abundance ($cell^{-1}$))")
 
     sns.barplot(x="Strategy", y="Selectivity", data=optDF, ax=ax[0])
     ax[0].set(title="Optimization of " + targetPop[0])
+    ax[0].set_xticklabels(ax.get_xticklabels(), rotation=25, horizontalalignment='right')
 
 
 optBnds = [(np.log(1e-11), np.log(1e-8)),  # log L0
