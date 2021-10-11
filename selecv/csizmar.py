050d31ab9175878ba8a9d070d30a58557a63dd95
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2021-10-11 12:18:17 -0700

    No Affinity Fit Inlcuded (#216)

--- selecv/csizmar.py
+++ selecv/csizmar.py
@@ -128,10 +128,15 @@ def resids(x):
     return np.linalg.norm(X2 - Y2) + np.linalg.norm(X1 - Y1)
 
 
-def fitfunc():
+def fitfunc(fitAff=True):
     "Runs least squares fitting for various model parameters, and returns the minimizers"
     x0 = np.array([np.log(10 ** -14.714), 0.01, 0.01, np.log(Kav[0])[0], np.log(Kav[1])[0], np.log(3.8e6), 1, 2, 4, 8])  # KXSTAR, slopeC5, slopeB22, KA C5, KA, B22, receps MH-7
-    bnds = ((None, None), (None, None), (None, None), (None, None), (None, None), (np.log(3.8e6) * 0.9999, np.log(3.8e6) * 1.0001), (1, 1.01), (2, 2.01), (4, 4.01), (8, 8.01))
+    if fitAff:
+        bnds = ((None, None), (None, None), (None, None), (None, None), (None, None), (np.log(3.8e6) * 0.9999, np.log(3.8e6) * 1.0001), (1, 1.01), (2, 2.01), (4, 4.01), (8, 8.01))
+    else:
+        bnds = ((None, None), (None, None), (None, None), (np.log(Kav[0])[0], np.log(Kav[0])[0] * 1.0001), (np.log(Kav[1])[0],
+                np.log(Kav[1])[0] * 1.0001), (np.log(3.8e6) * 0.9999, np.log(3.8e6) * 1.0001), (1, 1.01), (2, 2.01), (4, 4.01), (8, 8.01))
+
     parampredicts = minimize(resids, x0, jac="3-point", bounds=bnds)
     assert parampredicts.success
     return parampredicts.x
