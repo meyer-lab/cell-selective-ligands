9698746fc710a21d99df2e57fe0fe81f995e3e2c
cztan <cyztan@gmail.com>
2021-11-04 15:27:33 -0700

    More fix on Fig 3 cartoon

--- genFigures.py
+++ genFigures.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         overlayCartoon(fdir + 'figure1.svg',
                        './selecv/graphics/figure_1b.svg', 5, 280, scalee=0.12, scale_x=0.3, scale_y=0.3)
     if sys.argv[1] == '3':
-        overlayCartoon(fdir + 'figure5.svg',
-                       './selecv/graphics/figure_5i.svg', 480, 490, scalee=0.15, scale_x=1, scale_y=1)
+        overlayCartoon(fdir + 'figure3.svg',
+                       './selecv/graphics/figure_3i.svg', 480, 490, scalee=0.15, scale_x=1, scale_y=1)
 
     info('%s is done after %s seconds.', nameOut, time() - start)
