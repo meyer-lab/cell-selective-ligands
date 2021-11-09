5b013589356ccf9966eaad10f42d651ca8f50977
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2021-11-09 10:37:12 -0800

    Csizmar Done (#232)

--- selecv/figures/figureS2.py
+++ selecv/figures/figureS2.py
@@ -9,19 +9,13 @@ from ..csizmar import fit_slope, discrim2, fitfunc, xeno
 
 def makeFigure():
     # Get list of axis objects
-    ax, f = getSetup((12, 3), (1, 4))
+    ax, f = getSetup((9, 3), (1, 3))
     subplotLabel(ax)
-    fit = fitfunc()
+    fit = fitfunc(fitAff=False)
     FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund, valencies = np.exp(fit[0]), fit[1], fit[2], [[np.exp(fit[3])], [np.exp(fit[4])], [0]], np.exp(fit[5]), fit[6:10]
     fit_slope(ax[0], FitKX, FitSlopeC5, FitSlopeB22, Kav, Fitabund, valencies)
     discrim2(ax[1], FitKX, FitSlopeC5, FitSlopeB22, Kav, valencies[2:4])
     xeno(ax[2], FitKX, Kav, [valencies[3], valencies[2], valencies[3], valencies[2]])
-
-    # Fit without affinity
-    fitnA = fitfunc(fitAff=False)
-    FitKXnA, FitSlopeC5nA, FitSlopeB22nA, KavnA, FitabundnA, valenciesnA = np.exp(fitnA[0]), fitnA[1], fitnA[2], [[np.exp(fitnA[3])], [np.exp(fitnA[4])], [0]], np.exp(fitnA[5]), fitnA[6:10]
-
-    fit_slope(ax[3], FitKXnA, FitSlopeC5nA, FitSlopeB22nA, KavnA, FitabundnA, valenciesnA)
 
     #setFontSize(ax, 9)
     return f
